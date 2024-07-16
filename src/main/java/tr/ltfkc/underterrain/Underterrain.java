7014b26fec8d58237d6a397bc23632b0c6f27ec0
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-16 16:26:10 +0300

    Write testing fuctionality

--- src/main/java/tr/ltfkc/underterrain/Underterrain.java
+++ src/main/java/tr/ltfkc/underterrain/Underterrain.java
@@ -1,8 +1,13 @@
 package tr.ltfkc.underterrain;
 
+import javax.swing.*;
+
 public class Underterrain {
 
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("ads");
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
