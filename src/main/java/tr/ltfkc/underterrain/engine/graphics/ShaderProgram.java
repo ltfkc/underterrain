8d4214b3315e243e1fc3d0d82429932c2112dc5f
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-20 19:07:55 +0300

    Add ShaderProgram.java

---
+++ src/main/java/tr/ltfkc/underterrain/engine/graphics/ShaderProgram.java
@@ -0,0 +1,105 @@
+package tr.ltfkc.underterrain.engine.graphics;
+
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+
+import java.nio.FloatBuffer;
+
+public abstract class ShaderProgram {
+
+    private int programID;
+    private int vertexShaderID;
+    private int fragmentShaderID;
+
+    private static FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16); // 4 * 4
+
+    public ShaderProgram(String vertexSrc, String fragmentSrc) {
+        vertexShaderID = loadShader(vertexSrc, GL20.GL_VERTEX_SHADER);
+        fragmentShaderID = loadShader(fragmentSrc, GL20.GL_FRAGMENT_SHADER);
+        programID = GL20.glCreateProgram();
+        GL20.glAttachShader(programID, vertexShaderID);
+        GL20.glAttachShader(programID, fragmentShaderID);
+        bindAttributes();
+        GL20.glLinkProgram(programID);
+        GL20.glValidateProgram(programID);
+        getAllUniformLocations();
+    }
+
+    public void start() {
+        GL20.glUseProgram(programID);
+    }
+
+    public void stop() {
+        GL20.glUseProgram(0);
+    }
+
+    public void dispose() {
+        stop();
+        GL20.glDetachShader(programID, vertexShaderID);
+        GL20.glDetachShader(programID, fragmentShaderID);
+        GL20.glDeleteShader(vertexShaderID);
+        GL20.glDeleteShader(fragmentShaderID);
+        GL20.glDeleteProgram(programID);
+    }
+
+    protected abstract void bindAttributes();
+
+    protected void bindAttribute(int attribute, String variableName) {
+        GL20.glBindAttribLocation(programID, attribute, variableName);
+    }
+
+    protected void loadVector(int location, Vector4f vector) {
+        GL20.glUniform4f(location, vector.x, vector.y, vector.z, vector.w);
+    }
+
+    protected void loadInt(int location, int value) {
+        GL20.glUniform1i(location, value);
+    }
+
+    protected void loadFloat(int location, float value) {
+        GL20.glUniform1f(location, value);
+    }
+
+    protected void loadVector4f(int location, Vector4f vector) {
+        GL20.glUniform4f(location, vector.x, vector.y, vector.z, vector.w);
+    }
+
+    protected void loadVector(int location, Vector3f vector) {
+        GL20.glUniform3f(location, vector.x, vector.y, vector.z);
+    }
+
+    protected void load2DVector(int location, Vector2f vector) {
+        GL20.glUniform2f(location, vector.x, vector.y);
+    }
+
+    protected void loadBoolean(int location, boolean value) {
+        GL20.glUniform1f(location, value ? 1 : 0);
+    }
+
+    protected void loadMatrix(int location, Matrix4f matrix) {
+        matrix.get(matrixBuffer);
+        GL20.glUniformMatrix4fv(location, false, matrixBuffer);
+    }
+
+    private static int loadShader(String shaderSource, int type) {
+        int shaderID = GL20.glCreateShader(type);
+        GL20.glShaderSource(shaderID, shaderSource);
+        GL20.glCompileShader(shaderID);
+        if(GL20.glGetShaderi(shaderID, GL20.GL_COMPILE_STATUS) == GL11.GL_FALSE){
+            throw new RuntimeException("Could not compile shader.\n" + GL20.glGetShaderInfoLog(shaderID, 500));
+        }
+        return shaderID;
+    }
+
+    protected abstract void getAllUniformLocations();
+
+    protected int getUniformLocation(String uniformName) {
+        return GL20.glGetUniformLocation(programID, uniformName);
+    }
+
+}
