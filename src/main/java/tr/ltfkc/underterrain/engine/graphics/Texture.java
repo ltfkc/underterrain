37d1faab275d698802ad83ace2ca0a46f24c3e7c
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-20 19:07:28 +0300

    Add Texture.java

---
+++ src/main/java/tr/ltfkc/underterrain/engine/graphics/Texture.java
@@ -0,0 +1,31 @@
+package tr.ltfkc.underterrain.engine.graphics;
+
+import org.joml.Vector2f;
+
+public class Texture {
+
+    private int texID;
+    private int width, height;
+
+    public Texture(int texID, int width, int height) {
+        this.texID = texID;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getTextureID() {
+        return texID;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public Vector2f getDimensions() {
+        return new Vector2f(width, height);
+    }
+}
