946b266ff6b2357289aff874cf6505f2e7020a6b
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-21 17:25:17 +0300

    Remove tests

--- src/main/java/tr/ltfkc/underterrain/Underterrain.java
+++ src/main/java/tr/ltfkc/underterrain/Underterrain.java
@@ -14,38 +14,18 @@ public class Underterrain implements GameListener {
     Loader loader;
     Camera camera;
     Renderer renderer;
-    Renderable renderable;
-    Renderable renderable2;
-    Renderable renderable3;
 
     @Override
     public void create(Game game) {
         camera = new Camera(1200, 675);
         renderer = new Renderer();
-
         loader = new Loader();
-
-        renderable = new Renderable(new TexturedModel(loader, "/tile.png"), new Vector2f(32, 32), new Vector2f(1f, 1f), 0);
-        renderable3 = new Renderable(renderable.getTexturedModel(), new Vector2f(0, 0), new Vector2f(1f, 1f), 0);
-        renderable2 = new Renderable(new TexturedModel(loader, "/test.png"), new Vector2f(120, 120), new Vector2f(1f, 1f), 0);
     }
 
     @Override
     public void render(Game game) {
         renderer.prepare(Color.LIGHT_GRAY);
-
-        if (game.isKeyPressed(GLFW.GLFW_KEY_W))
-            renderable.getPosition().add(0, 200f * game.getDelta());
-        if (game.isKeyPressed(GLFW.GLFW_KEY_S))
-            renderable.getPosition().add(0, -200f * game.getDelta());
-        if (game.isKeyPressed(GLFW.GLFW_KEY_D))
-            renderable.getPosition().add(200f * game.getDelta(), 0);
-        if (game.isKeyPressed(GLFW.GLFW_KEY_A))
-            renderable.getPosition().add(-200f * game.getDelta(), 0);
-
-        renderer.put(renderable3);
-        renderer.put(renderable);
-        renderer.put(renderable2);
+        camera.update();
 
         renderer.render(camera);
     }
