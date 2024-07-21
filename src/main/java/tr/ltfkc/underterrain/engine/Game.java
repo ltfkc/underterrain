ba4694277d8c61bb13257f536848fcd34cafaccd
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-21 12:55:26 +0300

    Remove clearScreen() function from Game.java

--- src/main/java/tr/ltfkc/underterrain/engine/Game.java
+++ src/main/java/tr/ltfkc/underterrain/engine/Game.java
@@ -107,16 +107,6 @@ public class Game {
         this.gameListener.create(this);
     }
 
-    public void clearScreen(float r, float g, float b, float a) {
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-        GL11.glClearColor(r, g, b, a);
-    }
-
-    public void clearScreen(Color color) {
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-        GL11.glClearColor(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
-    }
-
     public void exit(int status) {
         terminate();
         System.exit(status);
