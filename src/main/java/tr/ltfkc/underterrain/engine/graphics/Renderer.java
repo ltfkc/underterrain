6fbe170c6d7e0d175ac80ad0ce7748fe0447fe07
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-21 17:24:47 +0300

    Edit Renderer.java

--- src/main/java/tr/ltfkc/underterrain/engine/graphics/Renderer.java
+++ src/main/java/tr/ltfkc/underterrain/engine/graphics/Renderer.java
@@ -8,18 +8,24 @@ import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 import org.joml.Matrix4f;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Renderer {
 
     private StaticShader shader = new StaticShader();
-    private Map<TexturedModel, List<Renderable>> renderables = new HashMap<>();
-    private Map<Renderable, Integer> priorityMap = new HashMap<>();
+    private Map<TexturedModel, HashMap<Renderable, Integer>> renderables = new HashMap<>();
 
     private int priorityCount = 0;
+
+    public Renderer() {
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
+        GL11.glAlphaFunc(GL11.GL_GREATER, 0f);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+    }
+
     public void prepare() {
         prepare(Color.BLACK);
     }
@@ -29,23 +35,26 @@ public class Renderer {
     }
 
     public void prepare(float r, float g, float b, float a) {
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         GL11.glClearColor(r, g, b, a);
     }
 
     public void put(Renderable renderable){
         TexturedModel texturedModel = renderable.getTexturedModel();
-        List<Renderable> batch = renderables.get(texturedModel);
+        HashMap<Renderable, Integer> batch = renderables.get(texturedModel);
         if (batch != null) {
-            batch.add(renderable);
+            batch.put(renderable, priorityCount);
         } else {
-            List<Renderable> newBatch = new ArrayList<>();
-            newBatch.add(renderable);
+            HashMap<Renderable, Integer> newBatch = new HashMap<>();
+            newBatch.put(renderable, priorityCount);
             renderables.put(texturedModel, newBatch);
         }
-        priorityMap.put(renderable, priorityCount);
         priorityCount++;
+    }
+
+    public void put(TexturedModel model, Vector2f position, Vector2f scale, float angle) {
+        Renderable renderable = new Renderable(model, position, scale, angle);
+        put(renderable);
     }
 
     public void render(Camera camera) {
@@ -54,19 +63,23 @@ public class Renderer {
         shader.loadProjectionMatrix(camera.getProjectionMatrix());
         for (TexturedModel model : renderables.keySet()) {
             prepareTexturedModel(model);
-            List<Renderable> batch = renderables.get(model);
-            for (Renderable renderable : batch) {
-                prepareInstance(renderable, priorityMap.get(renderable));
+            HashMap<Renderable, Integer> batch = renderables.get(model);
+            for (Renderable renderable : batch.keySet()) {
+                prepareInstance(renderable, batch.get(renderable));
                 GL11.glDrawElements(GL11.GL_TRIANGLES, model.getRawModel().getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
             }
             unbindTexturedModel();
         }
         shader.stop();
         renderables.clear();
+        priorityCount = 0;
     }
 
     public void dispose() {
         shader.dispose();
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
     }
 
     private void prepareTexturedModel(TexturedModel model) {
