dcd45c58b6fb33f2e5785c2e797088e248867823
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-27 14:12:47 +0300

    Write simple WASD movement

--- src/main/java/tr/ltfkc/underterrain/Underterrain.java
+++ src/main/java/tr/ltfkc/underterrain/Underterrain.java
@@ -1,8 +1,6 @@
 package tr.ltfkc.underterrain;
 
-import org.joml.Vector2f;
 import org.lwjgl.glfw.GLFW;
-
 import tr.ltfkc.underterrain.engine.Game;
 import tr.ltfkc.underterrain.engine.GameListener;
 import tr.ltfkc.underterrain.engine.graphics.*;
@@ -14,20 +12,40 @@ public class Underterrain implements GameListener {
     Loader loader;
     Camera camera;
     Renderer renderer;
+    TexturedModel model;
+
+    float x, y;
 
     @Override
     public void create(Game game) {
-        camera = new Camera(1200, 675);
+        camera = new Camera(game.getWindowWidth(), game.getWindowHeight());
         renderer = new Renderer();
         loader = new Loader();
+
+        model = new TexturedModel(loader, "/grass.png");
+
+        x = y = 0;
     }
 
     @Override
     public void render(Game game) {
-        renderer.prepare(Color.LIGHT_GRAY);
+        renderer.prepare(Color.DARK_GRAY);
         camera.update();
 
+        game.setWindowTitle("Underterrain | FPS " + game.getFPS());
+
+        y += game.isKeyPressed(GLFW.GLFW_KEY_W) ? 1 : 0;
+        y -= game.isKeyPressed(GLFW.GLFW_KEY_S) ? 1 : 0;
+        x += game.isKeyPressed(GLFW.GLFW_KEY_D) ? 1 : 0;
+        x -= game.isKeyPressed(GLFW.GLFW_KEY_A) ? 1 : 0;
+
+        renderer.put(model, x, y, model.getTexture().getWidth(), model.getTexture().getHeight(), 0);
         renderer.render(camera);
+    }
+
+    @Override
+    public void resize(Game game, int width, int height) {
+        camera.update(100, 100 * (float) height / width);
     }
 
     @Override
@@ -37,7 +55,7 @@ public class Underterrain implements GameListener {
     }
 
     public static void main(String[] args) {
-        Game game = new Game(1200, 675, "Underterrain", 60);
+        Game game = new Game(64 * 15, 9 * 15, "Underterrain", 144);
         game.setGameListener(new Underterrain());
         game.run();
     }
