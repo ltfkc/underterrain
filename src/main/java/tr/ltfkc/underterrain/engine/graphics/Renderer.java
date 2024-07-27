d96eea2879424924cec0cec16fde7f9ef797d5ed
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-27 14:10:25 +0300

    Make functions compatible with changes in Renderable.java

--- src/main/java/tr/ltfkc/underterrain/engine/graphics/Renderer.java
+++ src/main/java/tr/ltfkc/underterrain/engine/graphics/Renderer.java
@@ -52,15 +52,22 @@ public class Renderer {
         priorityCount++;
     }
 
-    public void put(TexturedModel model, Vector2f position, Vector2f scale, float angle) {
-        Renderable renderable = new Renderable(model, position, scale, angle);
+    public void put(TexturedModel model, float x, float y, float width, float height, float angle) { // 80
+        Renderable renderable = new Renderable(model, x + (width / 2f), y + (height / 2f), width, height, angle);
+        put(renderable);
+    }
+
+    public void put(TexturedModel model, float x, float y) {
+        float width = model.getTexture().getWidth();
+        float height = model.getTexture().getHeight();
+        Renderable renderable = new Renderable(model, x + (width / 2f), y + (height / 2f), width, height, 0);
         put(renderable);
     }
 
     public void render(Camera camera) {
         shader.start();
-        shader.loadViewMatrix(camera.getViewMatrix());
         shader.loadProjectionMatrix(camera.getProjectionMatrix());
+        shader.loadViewMatrix(camera.getViewMatrix());
         for (TexturedModel model : renderables.keySet()) {
             prepareTexturedModel(model);
             HashMap<Renderable, Integer> batch = renderables.get(model);
@@ -77,9 +84,6 @@ public class Renderer {
 
     public void dispose() {
         shader.dispose();
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glDisable(GL11.GL_ALPHA_TEST);
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
     }
 
     private void prepareTexturedModel(TexturedModel model) {
@@ -87,7 +91,6 @@ public class Renderer {
         GL30.glBindVertexArray(rawModel.getVaoID());
         GL20.glEnableVertexAttribArray(0);
         GL20.glEnableVertexAttribArray(1);
-        GL20.glEnableVertexAttribArray(2);
         GL13.glActiveTexture(GL13.GL_TEXTURE0);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getTextureID());
     }
@@ -95,23 +98,22 @@ public class Renderer {
     private void unbindTexturedModel() {
         GL20.glDisableVertexAttribArray(0);
         GL20.glDisableVertexAttribArray(1);
-        GL20.glDisableVertexAttribArray(2);
         GL30.glBindVertexArray(0);
     }
 
     private void prepareInstance(Renderable renderable, int priority) {
         Matrix4f transformationMatrix = createTransformationMatrix(renderable.getPosition(),
-                new Vector2f(renderable.getScale()).mul(renderable.getTexturedModel().getTexture().getDimensions()),
+                renderable.getDimensions(),
                 renderable.getAngle(), priority * (1f / priorityCount));
         shader.loadTransformationMatrix(transformationMatrix);
     }
 
-    private Matrix4f createTransformationMatrix(Vector2f translation, Vector2f scale, float angle, float zBuffer) {
+    private Matrix4f createTransformationMatrix(Vector2f translation, Vector2f dimensions, float angle, float zBuffer) {
         Matrix4f matrix = new Matrix4f();
         matrix.identity();
         matrix.translate(translation.x, translation.y, zBuffer);
         matrix.rotate(angle, new Vector3f(0, 0, 1));
-        matrix.scale(scale.x, scale.y, 64);
+        matrix.scale(dimensions.x, dimensions.y, 0);
         return matrix;
     }
 }
