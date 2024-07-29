1c7b25ae81f8799475d14f49741da41f12fe90b3
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-29 13:23:16 +0300

    Edit TexturedModel.java

--- src/main/java/tr/ltfkc/underterrain/engine/graphics/TexturedModel.java
+++ src/main/java/tr/ltfkc/underterrain/engine/graphics/TexturedModel.java
@@ -10,14 +10,6 @@ public class TexturedModel {
         this.texture = texture;
     }
 
-    public TexturedModel(Loader loader, String texturePath){
-        texture = loader.loadTexture(texturePath);
-        float[] vertices = {-0.5f, 0.5f, 0, -0.5f, -0.5f, 0, 0.5f, -0.5f, 0, 0.5f, 0.5f, 0};
-        int[] indices = {0, 1, 3, 3, 1, 2};
-        float[] textureCoords = {0, 0, 0, 1, 1, 1, 1, 0};
-        rawModel = loader.loadToVAO(vertices, textureCoords, indices);
-    }
-
     public RawModel getRawModel() {
         return rawModel;
     }
