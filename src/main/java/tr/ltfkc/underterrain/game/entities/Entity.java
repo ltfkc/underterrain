51de27d901dcef286b13a7dd313ee6ad84db5c42
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-29 13:50:03 +0300

    Remove old ecs implementation

--- src/main/java/tr/ltfkc/underterrain/game/entities/Entity.java
+++
@@ -1,6 +0,0 @@
-package tr.ltfkc.underterrain.game.entities;
-
-public abstract class Entity {
-
-    abstract void update(float delta);
-}
--- src/main/java/tr/ltfkc/underterrain/game/entities/traits/Renderable.java
+++
@@ -1,8 +0,0 @@
-package tr.ltfkc.underterrain.game.entities.traits;
-
-import tr.ltfkc.underterrain.engine.graphics.Renderer;
-
-public interface Renderable {
-
-    public void resolveRenderCall(Renderer renderer);
-}
