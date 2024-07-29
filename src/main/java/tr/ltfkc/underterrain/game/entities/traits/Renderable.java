dabf58446b050263277845e108b29a6b3877630f
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-29 13:43:09 +0300

    Add Renderable.java

---
+++ src/main/java/tr/ltfkc/underterrain/game/entities/traits/Renderable.java
@@ -0,0 +1,8 @@
+package tr.ltfkc.underterrain.game.entities.traits;
+
+import tr.ltfkc.underterrain.engine.graphics.Renderer;
+
+public interface Renderable {
+
+    public void resolveRenderCall(Renderer renderer);
+}
