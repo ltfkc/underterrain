ff0bd46853187f6fcf116fd9426f7376b71b2f8c
ltfkc <muhammedalitufekci08@gmail.com>
2024-07-29 13:42:51 +0300

    Fix Collidable.java

--- src/main/java/tr/ltfkc/underterrain/game/entities/traits/Collidable.java
+++ src/main/java/tr/ltfkc/underterrain/game/entities/traits/Collidable.java
@@ -2,9 +2,16 @@ package tr.ltfkc.underterrain.game.entities.traits;
 
 import org.joml.Vector2i;
 
-public abstract class Collidable {
+public interface Collidable {
 
-    public float collisionRectX, collisionRectY, collisionRectWidth, collisionRectHeight;
 
-    public abstract void resolveCollision(Vector2i direction);
+    void resolveCollision(Vector2i direction);
+
+    float getCollisionRectX();
+
+    float getCollisionRectY();
+
+    float getCollisionRectWidth();
+
+    float getCollisionRectHeight();
 }
